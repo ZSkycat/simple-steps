60bbed3260d0da95b46ec7d22320450b23360050
ZSkycat <skycatmew@gmail.com>
2018-09-12 07:38:46 +0800

    chore

--- src/index.ts
+++ src/index.ts
@@ -20,36 +20,36 @@ export class Steps {
         handleStepWhenStepsFail: true,
     };
 
-    options: StepsOptions;
-    stream: NodeJS.WriteStream;
-    isStarted = false;
-    stepList: Step[] = [];
-    tickBegin = 0;
-    tickEnd = 0;
-
-    get last() {
-        return this.stepList[this.stepList.length - 1];
-    }
-
-    get duration() {
-        if (this.isStarted) this.tickEnd = Date.now();
-        return (this.tickEnd - this.tickBegin) / 1000;
-    }
-
-    private get formatDuration() {
-        return this.options.duration ? ` in ${this.duration}s` : '';
-    }
-
     constructor(option?: Partial<StepsOptions>) {
         if (option) this.options = Object.assign({}, Steps.defaultOptions, option);
         else this.options = Object.assign({}, Steps.defaultOptions);
         this.stream = this.options.stream;
     }
 
+    options: StepsOptions;
+    stream: NodeJS.WriteStream;
+    isStarted = false;
+    stepList: Step[] = [];
+    private _tickBegin = 0;
+    private _tickEnd = 0;
+
+    get last() {
+        return this.stepList[this.stepList.length - 1];
+    }
+
+    get duration() {
+        if (this.isStarted) this._tickEnd = Date.now();
+        return (this._tickEnd - this._tickBegin) / 1000;
+    }
+
+    private get _formatDuration() {
+        return this.options.duration ? ` in ${this.duration}s` : '';
+    }
+
     step(title?: string) {
         if (this.stepList.length === 0) {
             this.isStarted = true;
-            this.tickBegin = Date.now();
+            this._tickBegin = Date.now();
         }
         let step = new Step(this, title);
         this.stepList.push(step);
@@ -59,7 +59,7 @@ export class Steps {
     stop() {
         if (this.isStarted) {
             this.isStarted = false;
-            this.tickEnd = Date.now();
+            this._tickEnd = Date.now();
         }
     }
 
@@ -68,7 +68,7 @@ export class Steps {
     succeed(handleStep: boolean = this.options.handleStepWhenStepsSucceed) {
         this.stop();
         if (handleStep) this.stepList.forEach(x => x.succeed());
-        this.stream.write(c.black.bgGreen(' succeed ') + c.green(this.formatDuration) + '\n');
+        this.stream.write(c.black.bgGreen(' succeed ') + c.green(this._formatDuration) + '\n');
     }
 
     // format:
@@ -76,52 +76,52 @@ export class Steps {
     fail(handleStep: boolean = this.options.handleStepWhenStepsFail) {
         this.stop();
         if (handleStep) this.stepList.forEach(x => x.fail());
-        this.stream.write(c.black.bgRed(' fail ') + c.red(this.formatDuration) + '\n');
+        this.stream.write(c.black.bgRed(' fail ') + c.red(this._formatDuration) + '\n');
     }
 }
 
 export type StepState = 'initial' | 'start' | 'stop' | 'succeed' | 'fail';
 
 export class Step {
+    constructor(context: Steps, title: string = '') {
+        this.context = context;
+        this.title = title;
+    }
+
     context: Steps;
     title: string;
     state: StepState = 'initial';
-    tickBegin = 0;
-    tickEnd = 0;
+    private _tickBegin = 0;
+    private _tickEnd = 0;
 
     get index() {
         return this.context.stepList.findIndex(x => x === this) + 1;
     }
 
     get duration() {
-        if (this.state === 'start') this.tickEnd = Date.now();
-        return (this.tickEnd - this.tickBegin) / 1000;
+        if (this.state === 'start') this._tickEnd = Date.now();
+        return (this._tickEnd - this._tickBegin) / 1000;
     }
 
     get isComplete() {
         return this.state === 'succeed' || this.state === 'fail';
     }
 
-    private get formatIndex() {
+    private get _formatIndex() {
         return `[${this.index}${this.context.options.stepTotal ? '/' + this.context.options.stepTotal : ''}]`;
     }
 
-    private get formatDuration() {
+    private get _formatDuration() {
         return this.context.options.duration ? c.gray(`(${this.duration}s)`) : '';
-    }
-
-    constructor(context: Steps, title: string = '') {
-        this.context = context;
-        this.title = title;
     }
 
     // format:
     // [0/0] > {title} ----------
     start() {
         if (this.state === 'initial') {
-            this.tickBegin = Date.now();
+            this._tickBegin = Date.now();
             this.state = 'start';
-            let msg = `${c.cyan(this.formatIndex)} ${c.cyan('>')} ${this.title}`;
+            let msg = `${c.cyan(this._formatIndex)} ${c.cyan('>')} ${this.title}`;
             if (this.context.options.splitLine) {
                 let columns = this.context.options.splitLine;
                 if (process.stdout.columns && process.stdout.columns < columns) columns = process.stdout.columns;
@@ -136,7 +136,7 @@ export class Step {
     stop() {
         if (this.state === 'start') {
             this.state = 'stop';
-            this.tickEnd = Date.now();
+            this._tickEnd = Date.now();
         }
     }
 
@@ -146,7 +146,7 @@ export class Step {
         if (this.isComplete) return;
         this.stop();
         this.state = 'succeed';
-        this.context.stream.write(`${c.green(this.formatIndex)} ${c.green('√')} ${title} ${this.formatDuration}` + '\n');
+        this.context.stream.write(`${c.green(this._formatIndex)} ${c.green('√')} ${title} ${this._formatDuration}` + '\n');
     }
 
     // format:
@@ -155,6 +155,6 @@ export class Step {
         if (this.isComplete) return;
         this.stop();
         this.state = 'fail';
-        this.context.stream.write(`${c.red(this.formatIndex)} ${c.red('×')} ${title} ${this.formatDuration}` + '\n');
+        this.context.stream.write(`${c.red(this._formatIndex)} ${c.red('×')} ${title} ${this._formatDuration}` + '\n');
     }
 }
